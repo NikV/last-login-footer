614a18136d71e5cf0b6cd73909d435982df2e73f
NikV <techvoltz@gmail.com>
2015-01-10 00:01:14 -0600

    MAJOR FIXES

--- last-login-footer.php
+++ last-login-footer.php
@@ -18,23 +18,23 @@ class How_Long_Logged_In {
 
 	}
 
+	//update_user_meta
 	public function last_user_login_time() {
 		$current_user = wp_get_current_user();
 		update_user_meta( $current_user->ID, 'user_last_login', time() );
 	}
 
-
-// Contents of the new admin list table column
+	//Adding on to admin footer
 	public function logged_in_time_footer( $footer_text ) {
 		$current_user = wp_get_current_user();
 
 		$meta = get_user_meta( $current_user->ID, 'user_last_login', time() );
-		$crap = sprintf( __( human_time_diff( $meta ) ) );
+		$time = sprintf( __( human_time_diff( $meta ) ) );
 
 		if ( is_admin() ) {
-			return str_replace( '</span>', '', $footer_text ) . ' | Hi Admin! You&#39;ve been logged in for ' . $crap . '</span>';
+			return str_replace( '</span>', '', $footer_text ) . ' | Hi Admin! You&#39;ve been logged in for ' . $time . '</span>';
 		} else {
-			return str_replace( '</span>', '', $footer_text ) . ' | Hi User! You&#39;ve been logged in for ' . $crap . '</span>';
+			return str_replace( '</span>', '', $footer_text ) . ' | Hi User! You&#39;ve been logged in for ' . $time . '</span>';
 
 		}
 
